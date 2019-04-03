c5b0c5700487acd9a873b63530c5df4151ccbc77
abutanda <abutanda@csub.ed>
2019-04-03 10:17:22 -0700

    parse:possible steps only

--- test.java
+++ test.java
@@ -4,41 +4,68 @@ import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
 
+import org.w3c.dom.*;
+import javax.xml.parsers.*;
+import java.io.*;
+
+import java.net.HttpURLConnection;
+import javax.xml.parsers.DocumentBuilderFactory;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
 //import wolfram Alpha 
 // ?
 
 public class test {
-  public static void main(String[] args)  {
-	String API_KEY = "LG7KVL-QJV2T43KWX" ;
-	String problem = "3x-7=11"; //need to get from camera
-	//String problemB = "x+7=11";
-	String updateS = problem.replace ("=","%3D");
-	updateS = updateS.replace ("+","%2B");
-	updateS = updateS.replace ("/","%2F");
-	System.out.println(updateS);
-	
-	String  urlString  = "http://api.wolframalpha.com/v2/query?appid="+ API_KEY + "&input=solve+"+updateS+"&output=xml&podstate=Result__Step-by-step+solution&format=plaintext";
-//3x-7%3D11
-	try {
-		StringBuilder result = new StringBuilder();
-		URL url = new URL(urlString); 
-		URLConnection conn = url.openConnection();
-		BufferedReader rd =  new BufferedReader(new InputStreamReader(conn.getInputStream()));
-		String line;
-		while ((line = rd.readLine()) != null){
-			result. append(line);
+	public static void main(String[] args)  {
+		String API_KEY = "LG7KVL-QJV2T43KWX" ;
+		String problem = "3x-7=11"; //need to get from camera
+		//String problemB = "x+7=11";
+		String updateS = problem.replace ("=","%3D");
+		updateS = updateS.replace ("+","%2B");
+		updateS = updateS.replace ("/","%2F");
+		System.out.println(updateS);
+
+		String  urlString  = "http://api.wolframalpha.com/v2/query?appid="+ API_KEY + "&input=solve+"+updateS+"&podstate=Result__Step-by-step+solution&format=plaintext";
+		System.out.println(urlString);
+		//3x-7%3D11
+
+
+		try {
+
+			StringBuilder result = new StringBuilder();
+			URL url = new URL(urlString); 
+			URLConnection conn = url.openConnection();
+			BufferedReader rd =  new BufferedReader(new InputStreamReader(conn.getInputStream()));
+			String line;
+			while ((line = rd.readLine()) != null){
+				result. append(line);
+			}
+			rd.close ();
+			//print out xml 
+			//System.out.println(result);
+			try{
+		    Document doc = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(new InputSource(new StringReader(result.toString())));
+				NodeList errNodes = doc.getElementsByTagName("queryresult");
+				if (errNodes.getLength() > 0) {
+					Element err = (Element)errNodes.item(0);
+					System.out.println("possible solution  -"+err.getElementsByTagName("plaintext").item(2).getTextContent());
+
+				} else { 
+					// success
+				}
+
+			}catch (Exception e){
+				System.out.println("Error parsing");
+			}
 		}
-		rd.close ();
-		System.out.println(result);
- 
+		catch(IOException e){
+			System.out.println(e.getMessage());
+		}
+
+
+
 	}
-	catch(IOException e){
-	System.out.println(e.getMessage());
 }
-
-  
-  }
-
-}
-  //URL url = new URL("INSERT YOUR SERVER REQUEST");
-//	System.out.println("Hello World");
+//https://chillyfacts.com/java-send-url-http-request-read-xml-response/
