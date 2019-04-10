a17f06cab8d7da159889c029aae23252542f6f9e
abutanda <abutanda@csub.ed>
2019-04-10 09:22:31 -0700

    adding user input

--- test.java
+++ test.java
@@ -15,8 +15,13 @@ import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
 import org.xml.sax.InputSource;
 
+import java.util.Scanner;  // Import the Scanner class
+
 public class test {
 	public static void main(String[] args)  {
+        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+        System.out.println("Enter solution");
+        String userSolution = myObj.nextLine();  // Read user input
 		String API_KEY = "LG7KVL-QJV2T43KWX" ;
 		String problem = "3x-7=11"; //need to get from camera
 		//String problemB = "x+7=11";
@@ -60,6 +65,7 @@ public class test {
 					System.out.println(temp);
 					//System.out.println(temp2);
 					System.out.println(solution);
+                    System.out.println("User Solution is: " + userSolution);  // Output user input 
 				}}catch (Exception e){
 					System.out.println("Error parsing");
 				}
